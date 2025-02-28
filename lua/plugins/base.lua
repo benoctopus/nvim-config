02bfd1b80ff6511f49a90a96b96ffa0378a4346c
b <rose.benjamin925@gmail.com>
2025-02-27 19:15:01 -0800

    feat: Add Kanagawa colorscheme and customize string and split colors

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -2,6 +2,28 @@
 -- stylua: ignore
 
 return {
+{
+    "LazyVim/LazyVim",
+    dependencies = {"rebelot/kanagawa.nvim"},
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
+  },
+  {
+      "rebelot/kanagawa.nvim",
+      opts = {
+        overrides = function()
+          return {
+            -- Change the color of strings
+            String = { fg = '#916546' }, -- Change this hex color to your preferred string color
+            -- You can also customize other syntax elements if needed
+            -- Add gray borders between splits
+            VertSplit = { fg = '#555555' }, -- Medium gray for vertical split borders
+            WinSeparator = { fg = '#555555' }, -- Same color for window separators
+          }
+        end,
+      },
+    },
   {
   "francoiscabrol/ranger.vim",
     cmd = {
@@ -16,52 +38,53 @@ return {
       { "<leader>ft", "<cmd>Ranger<cr>" },
     }
   },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-      }, 
-      {
-        "benfowler/telescope-luasnip.nvim"
-      }
-    },
-    config = function()
-      require("telescope").load_extension("fzf")
-      require("telescope").load_extension("luasnip")
-    end,
-    keys = {
-      {"<leader>/", false},
-      -- change a keymap
-      { "<leader>ff",
-        function()
-          local ok, _ = pcall(require("telescope.utils").get_git_root)
-          local opts = { hidden = true, file_ignore_patterns = { ".git/" } }
-          if ok then
-            opts.cwd = require("telescope.utils").get_git_root()
-          end
-          require('telescope.builtin').find_files(opts)
-        end,
-        desc = "Find Files",
-        silent = true,
-        noremap = true,
-      },
-      { 
-        "<leader>fg", 
-        function() require('telescope.builtin').live_grep() end, 
-        desc = "Live Grep",
-        silent = true,
-        noremap = true,
-      },
-      -- add a keymap to browse plugin files
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    }
-  },
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   dependencies = {
+  --     {
+  --       "nvim-telescope/telescope-fzf-native.nvim",
+  --       build = "make",
+  --     },
+  --     {
+  --       "benfowler/telescope-luasnip.nvim"
+  --     }
+  --   },
+  --   config = function()
+  --     require("telescope").load_extension("fzf")
+  --     require("telescope").load_extension("luasnip")
+  --   end,
+  --   keys = {
+  --     {"<leader>/", false},
+  --     -- change a keymap
+  --     { "<leader>ff",
+  --       function()
+  --         local opts = { hidden = true }
+  --         if vim.fn.finddir(".git", ".;") ~= "" then
+  --           require('telescope.builtin').git_files(opts)
+  --         else
+  --           opts.file_ignore_patterns = { ".git/" }
+  --           require('telescope.builtin').find_files(opts)
+  --         end
+  --       end,
+  --       desc = "Find Files",
+  --       silent = true,
+  --       noremap = true,
+  --     },
+  --     { 
+  --       "<leader>fg", 
+  --       function() require('telescope.builtin').live_grep() end, 
+  --       desc = "Live Grep",
+  --       silent = true,
+  --       noremap = true,
+  --     },
+  --     -- add a keymap to browse plugin files
+  --     {
+  --       "<leader>fp",
+  --       function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+  --       desc = "Find Plugin File",
+  --     },
+  --   }
+  -- },
   {
     "christoomey/vim-tmux-navigator",
     config = function ()
